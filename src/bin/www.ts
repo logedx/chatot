d5f8560ec9681b056b5c9f9e5672dad28c591ff3
leiddx <leiddx@hotmail.com>
2025-09-10 16:27:24 +0800

    ✨ feat(bin): improve readability of service startup information

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -80,37 +80,46 @@ async function read_file (name: string): Promise<Buffer>
 	let x = name.toLowerCase()
 	let y = name.toUpperCase()
 
-	console.group()
 
-	for (let v of [x, y])
+	try
 	{
-		try
-		{
-			return await fs.readFile(
-				path.resolve(app.cert_dirname, v),
+		return await Promise.any(
+			[
+				fs.readFile(
+					path.resolve(app.cert_dirname, x),
 
-			)
+				),
 
+				fs.readFile(
+					path.resolve(app.cert_dirname, y),
 
-		}
+				),
 
-		catch (e)
-		{
-			let z = e as NodeJS.ErrnoException
+			],
 
-			console.info(
-				chalk.red(z.message),
-
-			)
-
-
-		}
+		)
 
 	}
 
-	console.groupEnd()
+	catch (e)
+	{
+		console.group()
 
-	throw new Error(`${name} is not found`)
+		for (let z of (e as AggregateError).errors)
+		{
+			console.warn(
+				chalk.gray( (z as NodeJS.ErrnoException).message),
+
+			)
+
+		}
+
+		console.groupEnd()
+
+		throw new Error(`${name} is not found`)
+
+	}
+
 
 }
 
@@ -120,16 +129,21 @@ async function create_server
 	console.info()
 
 	console.info(
-		chalk.grey(`≥ Create HTTPS Server...`),
+		chalk.italic(`≥ Create HTTPS Server...`),
 
 	)
 
+	let settle = await Promise.allSettled(
+		[read_file(app.key_name), read_file(app.pem_name)],
 
-	try
+	)
+
+	if (settle.every(v => v.status === 'fulfilled') )
 	{
-		const key = await read_file(app.key_name)
-		const cert = await read_file(app.pem_name)
+		let [key, cert] = settle.map(
+			(v: PromiseFulfilledResult<Buffer>) => v.value,
 
+		)
 
 		return https.createServer(
 			{ key, cert },
@@ -140,31 +154,31 @@ async function create_server
 
 	}
 
-	catch (e)
+	console.info()
+
+
+	for (let e of settle.filter(v => v.status === 'rejected') )
 	{
-		let z = e as NodeJS.ErrnoException
-
-		console.info()
-
-		console.info(
-			chalk.gray(`× fail: ${z.message}`),
+		console.error(
+			chalk.red(`× ${e.reason}`),
 
 		)
-
-		console.info()
-		console.info()
-		console.info()
-		console.info()
-
-		console.info(
-			chalk.gray('≥ Create HTTP server...'),
-
-		)
-
-
-		return http.createServer(_app)
 
 	}
+
+	console.info()
+	console.info()
+	console.info()
+	console.info()
+
+	console.info(
+		chalk.italic('≥ Create HTTP server...'),
+
+	)
+
+
+	return http.createServer(_app)
+
 
 }
 
